c320cc4ea7c58bba667979b86dd3ff69aee6be1d
Brad Whiteside <bradwhiteside@protonmail.com>
2021-05-28 14:51:52 -0700

    Clean up code and attempt to add real time plotting of simulation

--- Plot_results.py
+++ Plot_results.py
@@ -1,55 +1,132 @@
+import _tkinter
 import matplotlib.pyplot as plt
 import numpy as np
-#Plot the path
-def plot_results(times, true_states, est_states, torques, speeds, accels, input_goal, yaw_goal, plt_pause = False, plt_show =False):
 
-    i_1 = -10000
-    i_2 = -1
-    plt.close()
-    fig1, ax1 = plt.subplots(3,2,figsize=(10,  7))
+
+def init_plot(plt_show):
+    if plt_show == False:
+        return None, None, None
+
+    fig1, axes = plt.subplots(3, 2, figsize=(10, 7))
     fig1.suptitle('x, y, z, roll, pitch, yaw', fontsize=16)
-    # ax1[0,0].plot(times, est_states[:,0], label = "x dir_est", color = "green")
-    ax1[0,0].plot(times[i_1:i_2], input_goal[i_1:i_2,0], label = "x goal")
-    ax1[0,0].plot(times[i_1:i_2], true_states[i_1:i_2,0], label = "x dir")
-    ax1[0,0].set_xlabel('time (s)')
-    ax1[0,0].set_ylabel('x (m)')
-    ax1[0,0].legend()
 
-    # ax1[1,0].plot(times, est_states[:,1], label = "y dir_est" , color = "green")
-    ax1[1,0].plot(times[i_1:i_2], input_goal[i_1:i_2,1], label = "y goal")
-    ax1[1,0].plot(times[i_1:i_2], true_states[i_1:i_2,1], label = "y dir")
-    ax1[1,0].set_xlabel('time (s)')
-    ax1[1,0].set_ylabel('y (m)')
-    ax1[1,0].legend()
+    axes[0, 0].set_xlabel('time (s)')
+    axes[0, 0].set_ylabel('x (m)')
+    #axes[0, 0].legend()
 
-    # ax1[2,0].plot(times, est_states[:,2], label = "z dir_est", color = "green")
-    ax1[2,0].plot(times[i_1:i_2], input_goal[i_1:i_2,2], label = "z goal")
-    ax1[2,0].plot(times[i_1:i_2], true_states[i_1:i_2,2], label = "z (altitude)")
-    ax1[2,0].set_xlabel('time (s)')
-    ax1[2,0].set_ylabel('z (m)')
-    # ax1[2,0].set_ylim([0,11])
-    ax1[2,0].legend()
+    axes[1, 0].set_xlabel('time (s)')
+    axes[1, 0].set_ylabel('y (m)')
+    #axes[1, 0].legend()
 
-    # ax1[0,1].plot(times, np.degrees(est_states[:,6]), label = "roll_est", color = "green")
-    ax1[0,1].plot(times[i_1:i_2], np.degrees(true_states[i_1:i_2,6]), label = "roll")
-    ax1[0,1].set_xlabel('time (s)')
-    ax1[0,1].set_ylabel('roll (deg)')
-    ax1[0,1].legend()
+    axes[2, 0].set_xlabel('time (s)')
+    axes[2, 0].set_ylabel('z (m)')
+    # axes[2,0].set_ylim([0,11])
+    #axes[2, 0].legend()
 
-    # ax1[1,1].plot(times, np.degrees(est_states[:,7]), label = "pitch_est", color = "green")
-    ax1[1,1].plot(times[i_1:i_2], np.degrees(true_states[i_1:i_2,7]), label = "pitch")
-    ax1[1,1].set_xlabel('time (s)')
-    ax1[1,1].set_ylabel('pitch (deg)')
-    ax1[1,1].legend()
+    axes[0, 1].set_xlabel('time (s)')
+    axes[0, 1].set_ylabel('roll (deg)')
+    #axes[0, 1].legend()
 
-    # ax1[2,1].plot(times, np.degrees(est_states[:,8]), label = "yaw_est", color = "green")
-    ax1[2,1].plot(times[i_1:i_2], np.degrees(yaw_goal[i_1:i_2]), label = "yaw goal")
-    ax1[2,1].plot(times[i_1:i_2], np.degrees(true_states[i_1:i_2,8]), label = "yaw")
-    ax1[2,1].set_xlabel('time (s)')
-    ax1[2,1].set_ylabel('yaw (deg)')
-    ax1[2,1].legend()
+    axes[1, 1].set_xlabel('time (s)')
+    axes[1, 1].set_ylabel('pitch (deg)')
+    #axes[1, 1].legend()
+
+    axes[2, 1].set_xlabel('time (s)')
+    axes[2, 1].set_ylabel('yaw (deg)')
+    #axes[2, 1].legend()
+    
+    dummy_1D_values = np.array([0])
+    dummy_state_values = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]).reshape((1, 9))
+    print(dummy_state_values)
+    lines = []
+
+    # axes[0,0].plot(times, est_states[:,0], label = "x dir_est", color = "green")
+    lines.extend(axes[0, 0].plot(dummy_1D_values, dummy_state_values[:, 0], label="x goal"))
+    lines.extend(axes[0, 0].plot(dummy_1D_values, dummy_state_values[:, 0], label="x dir"))
+
+    # axes[1,0].plot(times, est_states[:,1], label = "y dir_est" , color = "green")
+    lines.extend(axes[1, 0].plot(dummy_1D_values, dummy_state_values[:, 1], label="y goal"))
+    lines.extend(axes[1, 0].plot(dummy_1D_values, dummy_state_values[:, 1], label="y dir"))
+
+    # axes[2,0].plot(times, est_states[:,2], label = "z dir_est", color = "green")
+    lines.extend(axes[2, 0].plot(dummy_1D_values, dummy_state_values[:, 2], label="z goal"))
+    lines.extend(axes[2, 0].plot(dummy_1D_values, dummy_state_values[:, 2], label="z (altitude)"))
+
+    # axes[0,1].plot(times, np.degrees(est_states[:,6]), label = "roll_est", color = "green")
+    lines.extend(axes[0, 1].plot(dummy_1D_values, dummy_state_values[:, 6], label="roll"))
+
+    # axes[1,1].plot(times, np.degrees(est_states[:,7]), label = "pitch_est", color = "green")
+    lines.extend(axes[1, 1].plot(dummy_1D_values, dummy_state_values[:, 7], label="pitch"))
+
+    # axes[2,1].plot(times, np.degrees(est_states[:,8]), label = "yaw_est", color = "green")
+    lines.extend(axes[2, 1].plot(dummy_1D_values, dummy_1D_values, label="yaw goal"))
+    lines.extend(axes[2, 1].plot(dummy_1D_values, dummy_state_values[:, 8], label="yaw"))
+
+    return fig1, axes, lines
 
 
+# Plot the path
+def plot_results(figure, axes, lines, times, true_states, est_states, torques, speeds, accels, input_goal, yaw_goal,
+                 plt_pause=False):
+    if figure is None or axes is None or lines is None:
+        return
+
+    WINDOW_WIDTH = 0 if len(true_states[:, 0]) < 1000 else -1000
+    i = 0
+
+    # axes[0, 0].plot(times, est_states[:,0], label = "x dir_est", color = "green")
+    # axes[0, 0].plot(times[WINDOW_WIDTH:], input_goal[WINDOW_WIDTH:, 0], label="x goal")
+    # axes[0, 0].plot(times[WINDOW_WIDTH:], true_states[WINDOW_WIDTH:, 0], label="x dir")
+    lines[i].set_xdata(times[WINDOW_WIDTH:])
+    lines[i].set_ydata(input_goal[WINDOW_WIDTH:, 0])
+    i += 1
+    lines[i].set_data(times[WINDOW_WIDTH:], true_states[WINDOW_WIDTH:, 0])
+    i += 1
+
+    # axes[1, 0].plot(times, est_states[:,1], label = "y dir_est" , color = "green")
+    # axes[1, 0].plot(times[WINDOW_WIDTH:], input_goal[WINDOW_WIDTH:, 1], label="y goal")
+    # axes[1, 0].plot(times[WINDOW_WIDTH:], true_states[WINDOW_WIDTH:, 1], label="y dir")
+    lines[i].set_data(times[WINDOW_WIDTH:], input_goal[WINDOW_WIDTH:, 1])
+    i += 1
+    lines[i].set_data(times[WINDOW_WIDTH:], true_states[WINDOW_WIDTH:, 1])
+    i += 1
+
+    # axes[2, 0].plot(times, est_states[:,2], label = "z dir_est", color = "green")
+    # axes[2, 0].plot(times[WINDOW_WIDTH:], input_goal[WINDOW_WIDTH:, 2], label="z goal")
+    # axes[2, 0].plot(times[WINDOW_WIDTH:], true_states[WINDOW_WIDTH:, 2], label="z (altitude)")
+    lines[i].set_data(times[WINDOW_WIDTH:], input_goal[WINDOW_WIDTH:, 2])
+    i += 1
+    lines[i].set_data(times[WINDOW_WIDTH:], true_states[WINDOW_WIDTH:, 2])
+    i += 1
+
+    # axes[0, 1].plot(times, np.degrees(est_states[:,6]), label = "roll_est", color = "green")
+    # axes[0, 1].plot(times[WINDOW_WIDTH:], np.degrees(true_states[WINDOW_WIDTH:, 6]), label="roll")
+    lines[i].set_data(times[WINDOW_WIDTH:], np.degrees(true_states[WINDOW_WIDTH:, 6]))
+    i += 1
+
+    # axes[1, 1].plot(times, np.degrees(est_states[:,7]), label = "pitch_est", color = "green")
+    # axes[1, 1].plot(times[WINDOW_WIDTH:], np.degrees(true_states[WINDOW_WIDTH:, 7]), label="pitch")
+    lines[i].set_data(times[WINDOW_WIDTH:], np.degrees(true_states[WINDOW_WIDTH:, 7]))
+    i += 1
+
+    # axes[2, 1].plot(times, np.degrees(est_states[:,8]), label = "yaw_est", color = "green")
+    # axes[2, 1].plot(times[WINDOW_WIDTH:], np.degrees(yaw_goal[WINDOW_WIDTH:]), label="yaw goal")
+    # axes[2, 1].plot(times[WINDOW_WIDTH:], np.degrees(true_states[WINDOW_WIDTH:, 8]), label="yaw")
+    lines[i].set_data(times[WINDOW_WIDTH:], np.degrees(yaw_goal[WINDOW_WIDTH:]))
+    i += 1
+    lines[i].set_data(times[WINDOW_WIDTH:], np.degrees(true_states[WINDOW_WIDTH:, 8]))
+    i += 1
+
+    #if (plt_pause == True):
+      #  plt.pause(0.000000000000001)
+
+    try:
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+    except (_tkinter.TclError):
+        figure = None
+
+    """
     # fig2, ax2 = plt.subplots(3,2,figsize=(10,  7))
     # fig2.suptitle('v_x, v_y, v_z, roll_rate, pitch_rate, yaw_rate', fontsize=16)
     # ax2[0,0].plot(times, true_states[:,3], label = "x_vel")
@@ -87,8 +164,6 @@ def plot_results(times, true_states, est_states, torques, speeds, accels, input_
     # ax2[2,1].set_xlabel('time (s)')
     # ax2[2,1].set_ylabel('gamma_rate (rad/s)')
     # ax2[2,1].legend()
-
-
 
     # fig3, ax3 = plt.subplots(4,1, figsize=(10,  7))
     # fig3.suptitle('Torques, roll, pitch, yaw', fontsize=16)
@@ -131,11 +206,4 @@ def plot_results(times, true_states, est_states, torques, speeds, accels, input_
     # ax5[2].set_xlabel('time (s)')
     # ax5[2].set_ylabel('a_z (m/s^2)')
     # ax5[2].legend()
-
-    if (plt_pause == True):
-        plt.pause(0.000000000000001)
-    if (plt_show == True):
-        plt.show()
-
-
-
+    """
--- quad_sim.py
+++ quad_sim.py
@@ -1,69 +1,76 @@
-import quadcopter,gui, controller, estimator , Plotter
-from path_planner.path_planner import get_test_paths
+import argparse
+import datetime
 import signal
 import sys
-import datetime
-import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-from scipy.stats import norm
 from scipy import interpolate
-from Plot_results import plot_results
+import matplotlib.pyplot as plt
+import controller
+import estimator
+import gui
+import pycontroller
+import quadcopter
+from Plot_results import plot_results, init_plot
+from path_planner.path_planner import get_test_paths
 
 # Constants
-TIME_SCALING = 1.0 # Any positive number(Smaller is faster). 1.0->Real Time, 0.0->Run as fast as possible
-QUAD_DYNAMICS_UPDATE = 0.002 # seconds
-CONTROLLER_DYNAMICS_UPDATE = 0.005 # seconds
+TIME_SCALING = 1.0  # Any positive number(Smaller is faster). 1.0->Real Time, 0.0->Run as fast as possible
+QUAD_DYNAMICS_UPDATE = 0.002  # seconds
+CONTROLLER_DYNAMICS_UPDATE = 0.005  # seconds
 ESTIMATION_TIME_UPDATE = 0.005
 ESTIMATION_OBSERVATION_UPDATE = 0.1
 PLOTTER_UPDATE = 1.0
 run = True
 
-def Single_Point2Point(GOALS, goal_time_limit, tolorance):
-    start = GOALS[0]
 
+def Single_Point2Point(GOALS, goal_time_limit, tolerance):
+    start = GOALS[0]
     YAWS = [0] * len(GOALS)
 
+    plt.ion()
+    fig, axes, lines = init_plot(plt_show=True)
+
     # Define the quadcopters
-    QUADCOPTER={'q1':{'position': start,'orientation':[0,0,0],'L':0.5,'r':0.2,'prop_size':[21,9.5],'weight':7}} #w in kg, L and r in mm, prop_size in in
+    QUADCOPTER = {'q1': {'position': start, 'orientation': [0, 0, 0], 'L': 0.5, 'r': 0.2, 'prop_size': [21, 9.5],
+                         'weight': 7}}  # w in kg, L and r in mm, prop_size in in
 
     # Controller parameters Without estimator
-    CONTROLLER_PARAMETERS = {'Motor_limits':[1000, 45000],
-                        'Tilt_limits':[-2, 2],   #degrees
-                        'Yaw_Control_Limits':[-900,900],
-                        'Z_XY_offset':500,
-                        'Linear_PID':{'P':[500000,550000,72000],'I':[30,30,60],'D':[1500000,1200000,80000]},
-                        'Linear_To_Angular_Scaler':[1,1,0],
-                        'Yaw_Rate_Scaler':1.1,
-                        'Angular_PID':{'P':[7000,6500,3000],'I':[0,0,0],'D':[3000,3000,1200]},
-                        }
-    #CONTROLLER_PARAMETERS _if using estimator
-    # CONTROLLER_PARAMETERS = {'Motor_limits':[1000, 45000],
-    #                     'Tilt_limits':[-5, 5],   #degrees
-    #                     'Z_XY_offset':500,
-    #                     'Linear_PID':{'P':[1160000,1100000,30000],'I':[30,30,30],'D':[1500000,1500000,50000]},
-    #                     'Linear_To_Angular_Scaler':[1,1,0],
-    #                     'Yaw_Control_Limits':[-900,900],
-    #                     'Yaw_Rate_Scaler':1.1,
-    #                     'Angular_PID':{'P':[8000,8000,3000],'I':[0,0,0],'D':[2100,2100,1500]},
-    #                     }
+    CONTROLLER_PARAMETERS = {'Motor_limits': [1000, 45000],
+                             'Tilt_limits': [-2, 2],  # degrees
+                             'Yaw_Control_Limits': [-900, 900],
+                             'Z_XY_offset': 500,
+                             'Linear_PID': {'P': [500000, 550000, 72000], 'I': [30, 30, 60],
+                                            'D': [1500000, 1200000, 80000]},
+                             'Linear_To_Angular_Scaler': [1, 1, 0],
+                             'Yaw_Rate_Scaler': 1.1,
+                             'Angular_PID': {'P': [7000, 6500, 3000], 'I': [0, 0, 0], 'D': [3000, 3000, 1200]},
+                             }
+    # CONTROLLER_PARAMETERS _if using estimator
+    #  'Linear_PID':{'P':[1160000,1100000,30000],'I':[30,30,30],'D':[1500000,1500000,50000]},
+    #  'Angular_PID':{'P':[8000,8000,3000],'I':[0,0,0],'D':[2100,2100,1500]},
 
     # Catch Ctrl+C to stop threads
     signal.signal(signal.SIGINT, signal_handler)
+
     # Make objects for quadcopter, gui and controller
     quad = quadcopter.Quadcopter(QUADCOPTER)
-    est = estimator.EKF(quad.get_time, quad.get_position,quad.get_linear_rate, quad.get_linear_accelertaions, quad.get_IMU_accelertaions, quad.get_orientation, quad.get_Gyro, quad.get_state, quad.get_motor_speeds,quad.get_covariances, quad.get_GPS, params=CONTROLLER_PARAMETERS, quads=QUADCOPTER, quad_identifier='q1')
-    ctrl = controller.Controller_PID_Point2Point(quad.get_state,quad.get_time,quad.set_motor_speeds,est.get_estimated_state, quad.get_L, params=CONTROLLER_PARAMETERS,quad_identifier='q1')
-    plotter_obj = Plotter.plotter(quad.get_time)
+    est = estimator.EKF(quad.get_time, quad.get_position, quad.get_linear_rate, quad.get_linear_accelertaions,
+                        quad.get_IMU_accelertaions, quad.get_orientation, quad.get_Gyro, quad.get_state,
+                        quad.get_motor_speeds, quad.get_covariances, quad.get_GPS, params=CONTROLLER_PARAMETERS,
+                        quads=QUADCOPTER, quad_identifier='q1')
+    ctrl = controller.Controller_PID_Point2Point(quad.get_state, quad.get_time, quad.set_motor_speeds,
+                                                 est.get_estimated_state, quad.get_L, params=CONTROLLER_PARAMETERS,
+                                                 quad_identifier='q1')
+    # plotter_obj = Plotter.plotter(quad.get_time)
     # gui_object = gui.GUI(quads=QUADCOPTER)
 
     # Start the threads
-    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
-    ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
-    est.start_thread(time_update_rate=ESTIMATION_TIME_UPDATE, observation_update_rate = ESTIMATION_OBSERVATION_UPDATE ,time_scaling=TIME_SCALING)
+    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
+    ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
+    est.start_thread(time_update_rate=ESTIMATION_TIME_UPDATE, observation_update_rate=ESTIMATION_OBSERVATION_UPDATE,
+                     time_scaling=TIME_SCALING)
     # plotter_obj.start_thread(update_rate=PLOTTER_UPDATE, time_scaling=TIME_SCALING)
-    
+
     # Update the GUI while switching between destination poitions
     # output_file_name = "outputs/" + map.split('/')[2] + "_path_data.csv"
     # output_file = open(output_file_name, 'w', buffering=65536)
@@ -72,25 +79,24 @@ def Single_Point2Point(GOALS, goal_time_limit, tolorance):
     yaw_goal = np.empty(0)
 
     true_states = np.empty((0, 12), float)
-    est_states = np.empty((0,12), float)
-    torques = np.empty((0,4), float)
-    speeds = np.empty((0,6), float)
-    accels = np.empty((0,3),float)
+    est_states = np.empty((0, 12), float)
+    torques = np.empty((0, 4), float)
+    speeds = np.empty((0, 6), float)
+    accels = np.empty((0, 3), float)
 
     simulation_start_time = quad.get_time()
-    #Simulation
-    for goal,yaw in zip(GOALS,YAWS):
+    # Simulation
+    for goal, yaw in zip(GOALS, YAWS):
         print(goal)
         ctrl.update_target(goal)
         ctrl.update_yaw_target(yaw)
         goal_start_time = quad.get_time()
         time_laps = 0
         true_pos = np.array(quad.get_state('q1')[0:3])
-        est_state =  np.array(est.get_estimated_state('q1')[0:3])
+        est_state = np.array(est.get_estimated_state('q1')[0:3])
         dist = np.linalg.norm(est_state - goal)
-        
 
-        while  dist > tolorance or time_laps < goal_time_limit: #
+        while dist > tolerance or time_laps < goal_time_limit:
             # print("dist",dist)
             # print(t)
             # gui_object.quads['q1']['position'] = quad.get_position('q1')
@@ -98,27 +104,25 @@ def Single_Point2Point(GOALS, goal_time_limit, tolorance):
             # gui_object.update()
 
             true_state = np.array(quad.get_state('q1'))
-            est_state =  np.array(est.get_estimated_state('q1'))
+            est_state = np.array(est.get_estimated_state('q1'))
 
             true_states = np.append(true_states, np.array([true_state]), axis=0)
             est_states = np.append(est_states, np.array([est_state]), axis=0)
             torque = quad.get_tau()
-            torques = np.append(torques, np.array([torque]), axis = 0)
-            speeds = np.append(speeds, np.array([quad.get_motor_speeds('q1')]), axis = 0)
-            accels = np.append(accels, np.array([quad.get_linear_accelertaions('q1')]), axis = 0)
-
+            torques = np.append(torques, np.array([torque]), axis=0)
+            speeds = np.append(speeds, np.array([quad.get_motor_speeds('q1')]), axis=0)
+            accels = np.append(accels, np.array([quad.get_linear_accelertaions('q1')]), axis=0)
 
             time = quad.get_time()
-            times = np.append(times, np.array([(time-simulation_start_time).total_seconds()]), axis=0)
-            time_laps = (datetime.datetime.now()-goal_start_time).total_seconds() 
-            
+            times = np.append(times, np.array([(time - simulation_start_time).total_seconds()]), axis=0)
+            time_laps = (datetime.datetime.now() - goal_start_time).total_seconds()
+
             # dist = np.linalg.norm(true_state[0:3] - goal)
             dist = np.linalg.norm(est_state[0:3] - goal)
 
             input_goal = np.append(input_goal, np.array([goal]), axis=0)
             yaw_goal = np.append(yaw_goal, np.array([yaw]), axis=0)
-        plot_results(times, true_states, est_states, torques, speeds, accels, input_goal, yaw_goal, plt_pause = True, plt_show =False)
-
+        plot_results(fig, axes, lines, times, true_states, est_states, torques, speeds, accels, input_goal, yaw_goal, plt_pause=True)
 
     quad.stop_thread()
     ctrl.stop_thread()
@@ -129,38 +133,40 @@ def Single_Point2Point(GOALS, goal_time_limit, tolorance):
 
     np.savetxt('error_analysis/true_data.txt', true_states)
     np.savetxt('error_analysis/est_data.txt', est_states)
-    plot_results(times, true_states, est_states, torques, speeds, accels, input_goal, yaw_goal, plt_pause = False, plt_show =True)
+    # plot_results(times, true_states, est_states, torques, speeds, accels, input_goal, yaw_goal, plt_pause=False)
 
     return error
 
 
 def Single_Velocity():
     # Set goals to go to
-    GOALS = [(0.5,0,2),(0,0.5,2),(-0.5,0,2),(0,-0.5,2)]
+    GOALS = [(0.5, 0, 2), (0, 0.5, 2), (-0.5, 0, 2), (0, -0.5, 2)]
     # Define the quadcopters
-    QUADCOPTER={'q1':{'position':[0,0,0],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2}}
+    QUADCOPTER = {'q1': {'position': [0, 0, 0], 'orientation': [0, 0, 0], 'L': 0.3, 'r': 0.1, 'prop_size': [10, 4.5],
+                         'weight': 1.2}}
     # Controller parameters
-    CONTROLLER_PARAMETERS = {'Motor_limits':[4000,9000],
-                        'Tilt_limits':[-10,10],
-                        'Yaw_Control_Limits':[-900,900],
-                        'Z_XY_offset':500,
-                        'Linear_PID':{'P':[2000,2000,7000],'I':[0.25,0.25,4.5],'D':[50,50,5000]},
-                        'Linear_To_Angular_Scaler':[1,1,0],
-                        'Yaw_Rate_Scaler':0.18,
-                        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
-                        }
+    CONTROLLER_PARAMETERS = {'Motor_limits': [4000, 9000],
+                             'Tilt_limits': [-10, 10],
+                             'Yaw_Control_Limits': [-900, 900],
+                             'Z_XY_offset': 500,
+                             'Linear_PID': {'P': [2000, 2000, 7000], 'I': [0.25, 0.25, 4.5], 'D': [50, 50, 5000]},
+                             'Linear_To_Angular_Scaler': [1, 1, 0],
+                             'Yaw_Rate_Scaler': 0.18,
+                             'Angular_PID': {'P': [22000, 22000, 1500], 'I': [0, 0, 1.2], 'D': [12000, 12000, 0]},
+                             }
 
     # Catch Ctrl+C to stop threads
     signal.signal(signal.SIGINT, signal_handler)
     # Make objects for quadcopter, gui and controller
     quad = quadcopter.Quadcopter(QUADCOPTER)
     gui_object = gui.GUI(quads=QUADCOPTER)
-    ctrl = pycontroller.Controller_PID_Velocity(quad.get_state,quad.get_time,quad.set_motor_speeds,params=CONTROLLER_PARAMETERS,quad_identifier='q1')
+    ctrl = pycontroller.Controller_PID_Velocity(quad.get_state, quad.get_time, quad.set_motor_speeds,
+                                                params=CONTROLLER_PARAMETERS, quad_identifier='q1')
     # Start the threads
-    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
-    ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
+    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
+    ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
     # Update the GUI while switching between destination poitions
-    while(run==True):
+    while (run == True):
         for goal in GOALS:
             ctrl.update_target(goal)
             for i in range(150):
@@ -170,13 +176,18 @@ def Single_Velocity():
     quad.stop_thread()
     ctrl.stop_thread()
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Quadcopter Simulator")
     parser.add_argument("--sim", help='single_p2p, multi_p2p or single_velocity', default='single_p2p')
-    parser.add_argument("--time_scale", type=float, default=-1.0, help='Time scaling factor. 0.0:fastest,1.0:realtime,>1:slow, ex: --time_scale 0.1')
-    parser.add_argument("--quad_update_time", type=float, default=0.0, help='delta time for quadcopter dynamics update(seconds), ex: --quad_update_time 0.002')
-    parser.add_argument("--controller_update_time", type=float, default=0.0, help='delta time for controller update(seconds), ex: --controller_update_time 0.005')
+    parser.add_argument("--time_scale", type=float, default=-1.0,
+                        help='Time scaling factor. 0.0:fastest,1.0:realtime,>1:slow, ex: --time_scale 0.1')
+    parser.add_argument("--quad_update_time", type=float, default=0.0,
+                        help='delta time for quadcopter dynamics update(seconds), ex: --quad_update_time 0.002')
+    parser.add_argument("--controller_update_time", type=float, default=0.0,
+                        help='delta time for controller update(seconds), ex: --controller_update_time 0.005')
     return parser.parse_args()
+
 
 def signal_handler(signal, frame):
     global run
@@ -186,53 +197,31 @@ def signal_handler(signal, frame):
 
 
 if __name__ == "__main__":
+    paths = get_test_paths(venue="Test")
 
-    startx =19 
-    starty =  24
-    endx  = 211
-    endy = 20
-    paths = get_test_paths(venue = "Test")
-    path1 = paths[((startx, starty), (endx, endy))]
-    print("path shape is:", path1.shape)
+    for path_index, GOALS in paths.items():
+        x = GOALS[:, 0]
+        y = GOALS[:, 1]
+        z = GOALS[:, 2]
+        input_range = np.arange(0, len(x))
+        f_x = interpolate.interp1d(input_range, x)
+        f_y = interpolate.interp1d(input_range, y)
+        f_z = interpolate.interp1d(input_range, z)
 
-    
-    x = path1[:,0]
-    y = path1[:,1]
-    z = path1[:,2]
-    input_range = np.arange(0,len(x))
-    f_x = interpolate.interp1d(input_range, x)
-    f_y = interpolate.interp1d(input_range, y)
-    f_z = interpolate.interp1d(input_range, z)
+        data_points = 400
+        new_range = np.linspace(0, len(input_range) - 1, data_points)
+        x_new = f_x(new_range)
+        y_new = f_y(new_range)
+        z_new = f_z(new_range)
+        INTERPOLATED_GOALS = np.vstack((x_new, y_new, z_new)).T
+        print("New Goal shape is:", INTERPOLATED_GOALS.shape)
 
-    data_points = 400
-    new_range = np.linspace(0,len(input_range)-1, data_points)
-    x_new = f_x(new_range)
-    y_new = f_y(new_range)
-    z_new = f_z(new_range)
-    GOALS = np.vstack((x_new,y_new,z_new)).T 
-    print("New Goal shape is:",GOALS.shape)
+        number_of_trials = 1
+        goal_time_limit = 2  # Amount of time limit to spend on a Goal
+        tolerance = 1.5  # Steady state error
 
-
-    #Ramp input
-    # x_ramp = np.linspace(0,10,10)
-    # x_ramp = np.hstack((x_ramp,np.linspace(10,0,10)))
-    # y_ramp = np.linspace(0,20,20)
-    # # y_ramp = np.hstack((y_ramp,np.linspace(20,0,10)))
-    # z_ramp = np.linspace(5,5,20)
-    # GOALS = np.vstack((x_ramp,y_ramp,z_ramp)).T 
-    # YAWS = np.hstack((np.linspace(0, np.pi/4,10)))#, np.linspace(0, 0, 10)))
-    # YAWS = [0,0,0, np.pi/4,np.pi/4, np.pi/2,np.pi/2, 0.7 * np.pi, 0.7 *np.pi, 0.7 * np.pi, 0,0,0,0,0]
-
-
-    #for idx in paths:
-        #paths[idx]
-
-    number_of_trials = 1
-    goal_time_limit = 2  #Amount of time limit to spend on a Goal            
-    tolorance = 3                   #Steady state error
-
-    error = Single_Point2Point(GOALS = GOALS, goal_time_limit = goal_time_limit, tolorance =tolorance)
-    # print("error shape is:", error.shape[0])
+        error = Single_Point2Point(GOALS=GOALS, goal_time_limit=goal_time_limit, tolerance=tolerance)
+        # print("error shape is:", error.shape[0])
 
     # for i in range(0,number_of_trials-1):
     #     print("Trial", i)
@@ -240,7 +229,7 @@ if __name__ == "__main__":
     #     error_len = error.shape[0]
     #     new_error_leng = new_error.shape[0]
     #     diff = np.abs(new_error_leng - error_len)
-        
+
     #     if new_error_leng > error_len :
     #         # new_error = new_error[:-diff]
     #         # error += new_error #np.concatenate((error, new_error))
@@ -253,14 +242,12 @@ if __name__ == "__main__":
     #         np.concatenate((error, new_error))
     #         print('error shranked by {} elements'.format(diff))
 
-
     #     # print("Error length is:",new_error_leng)
     #     # error = np.concatenate((error, Single_Point2Point()))
 
     # #error = error/number_of_trials
     # print(error.shape)
     # np.savetxt('error_analysis/errors.txt', error)
-
 
     # args = parse_args()
     # if args.time_scale>=0: TIME_SCALING = args.time_scale
